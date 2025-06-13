1a81539d575e4a82810dbfafd7a3687e203aa412
AliemVanHou <renaldzvanhou@gmail.com>
2025-06-13 19:09:29 +0700

    Add files via upload

---
+++ reinaldiAlimsya_17_andal.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+garis()
+{
+	printf("\n------------------\n");
+}
+
+main() {
+		garis();
+		cout << "AMIK BSI -  - PONDOK LABU" << endl; 
+		garis();
+		getche();
+}
